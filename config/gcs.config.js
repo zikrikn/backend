2dfddaac1983bbde6465fcfe7113036c896f16e9
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2023-09-30 15:49:17 +0700

    add new env

--- config/gcs.config.js
+++ config/gcs.config.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
   credentials: {
-    private_key: process.env.GCP_PRIVATE_KEY,
+    private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.GCP_CLIENT_EMAIL,
   },
 });
