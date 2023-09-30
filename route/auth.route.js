12287235d1f92d24299f2c0f34e6afd2e6426f95
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2023-09-30 11:49:46 +0700

    fix structure files

--- route/auth.route.js
+++ route/auth.route.js
@@ -1,13 +1,12 @@
 const { Signup, Login, Logout, Profile, UpdateProfile } = require("../controller/auth.controller");
 const router = require("express").Router();
 const { verifyToken } = require("../middleware/auth.middleware");
-const { uploadPhotoProfile, uploadPicture } = require("../middleware/upload.middleware");
+const { uploadPhotoProfile } = require("../middleware/upload.middleware");
 
 router.post("/signup", Signup);
 router.post("/login", Login);
 router.get("/logout", Logout);
 router.put("/profile", verifyToken, uploadPhotoProfile, UpdateProfile);
 router.get("/profile", verifyToken, Profile);
-// router.put("/picture", verifyToken, uploadPicture, UpdatePicture);
 
 module.exports = router;
--- util/gcs.util.js
+++ util/gcs.util.js
@@ -1,24 +1,27 @@
-const util = require('util');
-const gc = require('../config/gcs.config');
+const util = require("util");
+const gc = require("../config/gcs.config");
 const bucket = gc.bucket(process.env.GCP_BUCKET_NAME);
 
-const uploadImage = (file) => new Promise((resolve, reject) => {
-  const { originalname, buffer } = file;
-
-  const blob = bucket.file(originalname.replace(/ /g, "_"));
-  const blobStream = blob.createWriteStream({
-    resumable: false
-  });
-  blobStream.on('finish', () => {
+const uploadImage = async (file) => {
+    const { originalname, buffer } = file;
+  
+    const blob = bucket.file(originalname.replace(/ /g, "_"));
+    const blobStream = blob.createWriteStream({
+      resumable: false
+    });
+  
+    // Write the image data to the blob stream
+    await blobStream.write(buffer);
+  
+    // Wait for the blob stream to finish writing
+    await blobStream.finished;
+  
+    // Generate the public URL for the uploaded image
     const publicUrl = util.format(
       `https://storage.googleapis.com/${bucket.name}/${blob.name}`
     );
-    resolve(publicUrl);
-  })
-  .on('error', () => {
-    reject(`Unable to upload image, something went wrong`);
-  })
-  .end(buffer);
-});
+  
+    return publicUrl;
+  };
 
 module.exports = uploadImage;
