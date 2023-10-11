9c9daa2f6e07e67a2a3c9db002632a5faba3a001
M Yardan Aqiella <myardan.aqiella22@gmail.com>
2023-10-11 07:47:45 +0700

    change cors origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: true,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
